ac3037129f03a9e329794e0bfca3bac1ec7da4af
KalpaOfficial <kalpadsilvaofficial@gmail.com>
2026-02-24 22:50:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const myModule = require('./my-module.js');
-console.log(myModule.myFunction());
-const fs = require('fs');
-fs.readFile('file.txt', 'utf8', function (err, data) {
- if (err) throw err;
- console.log(data);
+const myPromise = new Promise((resolve, reject) => {
+ if (condition) {
+ resolve('Success!');
+ } else {
+ reject('Failure!');
+ }
+});
+myPromise.then((result) => {
+ console.log(result);
+}).catch((error) => {
+ console.log(error);
 });
